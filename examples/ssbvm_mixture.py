ca4750b898e337bb8f81fff6b2652569c49d44e9
Ola <ronning@protonmail.com>
2021-05-29 13:00:49 +0200

    Skeleton for skew procedure.

--- examples/ssbvm_mixture.py
+++ examples/ssbvm_mixture.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import pickle
 from math import pi
 from pathlib import Path
@@ -9,7 +10,7 @@ from jax import random
 import numpyro
 from numpyro.contrib.funsor import config_enumerate
 from numpyro.distributions import Dirichlet, Gamma, Uniform, VonMises, Beta, Categorical, Sine
-from numpyro.infer import NUTS, init_to_median, MCMC
+from numpyro.infer import NUTS, MCMC, init_to_sample
 
 AMINO_ACIDS = ['M', 'N', 'I', 'F', 'E', 'L', 'R', 'D', 'G', 'K', 'Y', 'T', 'H', 'S', 'P', 'A', 'V', 'Q', 'W', 'C']
 
@@ -17,7 +18,7 @@ AMINO_ACIDS = ['M', 'N', 'I', 'F', 'E', 'L', 'R', 'D', 'G', 'K', 'Y', 'T', 'H', 
 @config_enumerate
 def ss_model(data, num_mix_comp=2):
     # Mixture prior
-    mix_weights = numpyro.sample('mix_weights', Dirichlet(0.5 * jnp.ones((num_mix_comp,))))
+    mix_weights = numpyro.sample('mix_weights', Dirichlet(jnp.ones((num_mix_comp,))))
 
     # Hprior BvM
     # Bayesian Inference and Decision Theory by Kathryn Blackmond Laskey
@@ -36,26 +37,19 @@ def ss_model(data, num_mix_comp=2):
         psi_conc = numpyro.sample('psi_conc', Beta(halpha_psi, beta_prec_psi - halpha_psi))
         corr_scale = numpyro.sample('corr_scale', Beta(2., 5.))
 
-        # SS prior
-        skew_phi = numpyro.sample('skew_phi', Uniform(-1., 1.))
-        psi_bound = 1 - jnp.abs(skew_phi)
-        skew_psi = numpyro.sample('skew_psi', Uniform(-1., 1.))
-        skewness = jnp.stack((skew_phi, psi_bound * skew_psi), axis=-1)
-        assert skewness.shape == (num_mix_comp, 2)
-
     with numpyro.plate('obs_plate', len(data), dim=-1):
         assign = numpyro.sample('mix_comp', Categorical(mix_weights), infer={"enumerate": "parallel"})
         sine = Sine(phi_loc=phi_loc[assign], psi_loc=psi_loc[assign],
-                    phi_concentration=1000 * phi_conc[assign],
-                    psi_concentration=1000 * psi_conc[assign],
+                    phi_concentration=500 * phi_conc[assign] + 200,
+                    psi_concentration=500 * psi_conc[assign] + 200,
                     weighted_correlation=corr_scale[assign])
         return numpyro.sample('phi_psi', sine, obs=data)
 
 
 def run_hmc(model, data, num_mix_comp, num_samples):
     rng_key = random.PRNGKey(0)
-    kernel = NUTS(model, init_strategy=init_to_median())
-    mcmc = MCMC(kernel, num_samples=num_samples, num_warmup=num_samples // 3)
+    kernel = NUTS(model, init_strategy=init_to_sample())
+    mcmc = MCMC(kernel, num_samples=num_samples, num_warmup=num_samples // 2)
     mcmc.run(rng_key, data, num_mix_comp)
     mcmc.print_summary()
     post_samples = mcmc.get_samples()
@@ -73,12 +67,13 @@ def fetch_aa_dihedrals(split='train', subsample_to=1000_000):
     return data
 
 
-def main(num_mix_comp=2, num_samples=200, aas=('S', 'P', 'G'),
+def main(num_mix_comp=4, num_samples=100, aas=('S', 'P', 'G'),
          show_viz=False, use_cuda=False, report_waic=False, capture_std=False, rerun_inference=False, report_bf=False):
     chain_file = Path(__file__).parent / f'ssbvm_bmixture_comp{num_mix_comp}_steps{num_samples}.pkl'
 
-    data = fetch_aa_dihedrals(subsample_to=100)
-    posterior_samples = {aa: {'ss': run_hmc(ss_model, data[aa], num_mix_comp, num_samples)} for aa in aas}
+    data = fetch_aa_dihedrals(subsample_to=50_000)
+    pos = {aa: {'ss': run_hmc(ss_model, data[aa], num_mix_comp, num_samples)} for aa in aas}
+
 
 
 if __name__ == '__main__':
--- numpyro/distributions/directional.py
+++ numpyro/distributions/directional.py
@@ -231,6 +231,81 @@ def _projected_normal_log_prob_2(concentration, value):
 
     return para_part + perp_part
 
+class SineSkewed(Distribution):
+    """The Sine Skewed distribution [1] is a distribution for breaking pointwise-symmetry on a base-distribution over
+    the d-dimensional torus defined as ⨂^d S^1 where S^1 is the circle. So for example the 0-torus is a point, the
+    1-torus is a circle and the 2-tours is commonly associated with the donut shape (some may object to this simile).
+
+    The skewness parameter can be inferred using :class:`~pyro.infer.HMC` or :class:`~pyro.infer.NUTS`.
+    For example, the following will produce a uniform prior over skewness for the 2-torus,::
+
+        def model(...):
+            ...
+            skew_phi = pyro.sample(f'skew_phi', Uniform(-1., 1.))
+            psi_bound = 1 - skewness_phi.abs()
+            skew_psi = pyro.sample(f'skew_psi', Uniform(-1, 1.))
+            skewness = torch.stack((skew_phi, psi_bound * skew_psi), dim=0)
+            ...
+
+    In the context of :class:`~pyro.infer.SVI`, this distribution can be freely used as a likelihood, but use as a
+    latent variables will lead to slow inference for 2 and higher order toruses. This is because the base_dist
+    cannot be reparameterized.
+
+    .. note:: An event in the base distribution must be on a d-torus, so the event_shape must be (d,).
+
+    .. note:: For the skewness parameter, it must hold that the sum of the absolute value of its weights for an event
+        must be less than or equal to one. See eq. 2.1 in [1].
+
+    ** References: **
+      1. Sine-skewed toroidal distributions and their application in protein bioinformatics
+         Ameijeiras-Alonso, J., Ley, C. (2019)
+
+    :param base_dist: base density on a d-dimensional torus.
+    :param skewness: skewness of the distribution.
+    """
+    arg_constraints = {'skewness': constraints.independent(constraints.interval(-1., 1.), 1)}
+
+    support = constraints.independent(constraints.real, 1)
+
+    def __init__(self, base_dist: Distribution, skewness, validate_args=None):
+        if (skewness.abs().sum(-1) > 1.).any():
+            warnings.warn("Total skewness weight shouldn't exceed one.", UserWarning)
+
+        batch_shape = broadcast_shapes(base_dist.batch_shape, skewness.shape[:-1])
+        event_shape = skewness.shape[-1:]
+        self.skewness = skewness.broadcast_to(batch_shape + event_shape)
+        self.base_dist = base_dist.expand(batch_shape)
+        super().__init__(batch_shape, event_shape, validate_args=validate_args)
+
+        if self._validate_args and base_dist.mean.device != skewness.device:
+            raise ValueError(f"base_density: {base_dist.__class__.__name__} and SineSkewed "
+                             f"must be on same device.")
+
+    def __repr__(self):
+        args_string = ', '.join(['{}: {}'.format(p, getattr(self, p)
+                                if getattr(self, p).numel() == 1
+                                else getattr(self, p).size()) for p in self.arg_constraints.keys()])
+        return self.__class__.__name__ + '(' + f'base_density: {str(self.base_dist)}, ' + args_string + ')'
+
+    def sample(self, key, sample_shape=()):
+        base_key, skew_key = random.split(key)
+        bd = self.base_dist
+        ys = bd.sample(base_key, sample_shape)
+        u = random.uniform(skew_key,sample_shape + self.batch_shape, 0., 1., )
+
+        # Section 2.3 step 3 in [1]
+        mask = u <= .5 + .5 * (self.skewness * jnp.sin((ys - bd.mean) % (2 * pi))).sum(-1)
+        mask = mask[..., None]
+        samples = (jnp.where(mask, ys, -ys + 2 * bd.mean) + pi) % (2 * pi) - pi
+        return samples
+
+    def log_prob(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
+
+        # Eq. 2.1 in [1]
+        skew_prob = jnp.log(1 + (self.skewness * jnp.sin((value - self.base_dist.mean) % (2 * pi))).sum(-1))
+        return self.base_dist.log_prob(value) + skew_prob
 
 class Sine(Distribution):
     r""" Unimodal distribution of two dependent angles on the 2-torus (S^1 ⨂ S^1) given by
